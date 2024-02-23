09b0ff3c1754f0146013a367c34119da445b7000
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-02-23 13:10:29 -0800

    add: test scripts

---
+++ test.ps1
@@ -0,0 +1,7 @@
+& "$((Get-Command -Name "pyenv").Source)" shell "3.12.2"
+
+Push-Location -Location (Join-Path -Path $PScriptDir -ChildPath "test")
+
+& "$((Get-Command -Name "python").Source)" "$(Join-Path -Path $PWD -ChildPath "test.py")";
+
+Pop-Location
